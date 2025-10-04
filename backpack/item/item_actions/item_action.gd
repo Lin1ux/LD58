3a507b46d59b4d63883e41527917033579d4df04
shamotte <123325697+shamotte@users.noreply.github.com>
2025-10-04 17:51:09 +0200

    started working on itmes

---
+++ backpack/item/item_actions/item_action.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name ItemAction
+
+
+func execute(params : CastParams):
+	printerr("executed empty action")
---
+++ backpack/item/item_effect.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name ItemEffect
+
+
+@export var effects : Array[ItemAction]
