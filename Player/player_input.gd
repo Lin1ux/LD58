7f06cb4747dd4943568d77ed2389dacbdb1f29f2
shamotte <123325697+shamotte@users.noreply.github.com>
2025-10-04 22:20:38 +0200

    some changes

---
+++ Player/player_input.gd
@@ -0,0 +1,19 @@
+extends Node
+class_name PlayerInput
+
+func _input(event: InputEvent) -> void:
+	if event.is_pressed():
+		if event.is_action("primary_action"):
+			action_primary.emit()
+		elif event.is_action("secondary_action"):
+			action_secondary.emit()
+		elif event.is_action("tetiary_action"):
+			action_tetiary.emit()
+
+
+
+
+
+signal action_primary
+signal action_secondary
+signal action_tetiary
