fb5a3f66551230659d9e2d70d30f5c498b22746e
Liniux <153652735+Lin1ux@users.noreply.github.com>
2025-10-04 15:06:02 +0200

    fix player

--- Player/player_movement.gd
+++ Player/player_movement.gd
@@ -5,9 +5,10 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_left", "ui_right")
+	if direction_x:
+		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
