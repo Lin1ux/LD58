abdbe5a6362ccca11b12b6ec33f7d5be54dc030c
Lin1ux <liniuxpl2@gmail.com>
2025-10-06 01:56:13 +0200

    add dealing dmg

--- components/melee_attacker/enemy_melee_attacker.gd
+++ components/melee_attacker/enemy_melee_attacker.gd
@@ -9,5 +9,8 @@ func attack(size : Vector2 = Vector2.ONE * 200, callback : Callable = func x() :
 	collision_shape_2d.shape.size = size
 	attack_area.position.x = size.x/2
 	look_at(GameInfo.player.global_position)
-	for x in attack_area.get_overlapping_bodies():
-		print("hitting ", x.name)
+	for x in attack_area.get_overlapping_areas():
+		var hp_component : HpComponent = x as HpComponent
+		if hp_component != null:
+			hp_component.health.decrease_hp(enemy.stats.dmg)
+		#print("hitting ", x.name)
