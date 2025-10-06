cd9f8ceba31f6ad9fb60980bc75dd37b2bc8ceb9
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-10-06 22:06:53 +0200

    Export preparations

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -21,39 +21,39 @@ func _ready():
 	hp.init(stats.hp)
 	hp.dead.connect(kill)
 	hp.damage_dealt.connect(get_damage)
-	
+
 func move():
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
 	play_animation_based_on_direction(dir)
 	velocity = dir * stats.speed
 	move_and_slide()
-	
+
 func make_path_to_player():
-	nav_agent.target_position = GameInfo.player.global_position 
-	
+	nav_agent.target_position = GameInfo.player.global_position
+
 func make_path_to_point(new_position :Vector2):
 	nav_agent.target_position = new_position
-	
+
 func heal(value : int):
 	hp.increase_hp(value)
-	
+
 func play_animation_based_on_direction(dir : Vector2):
 	if animSprite != null:
 		animSprite.scale.x = 1
 		match Direction.get_direction(dir):
-			Direction.Dir.UP: 
+			Direction.Dir.UP:
 				animSprite.play("walk_up")
-			Direction.Dir.DOWN: 
+			Direction.Dir.DOWN:
 				animSprite.play("walk_down")
-			Direction.Dir.LEFT: 
+			Direction.Dir.LEFT:
 				if stats.can_flip_sprite:
 					animSprite.play("walk_right")
 					animSprite.scale.x = -1
 				else:
 					animSprite.play("walk_left")
-			Direction.Dir.RIGHT: 
+			Direction.Dir.RIGHT:
 				animSprite.play("walk_right")
-				
+
 func attack():
 	if attack_component is Enemy_Projectile_Spawner:
 		attack_component.spawn_projectile(stats.attack_pattern)
@@ -64,9 +64,9 @@ func attack():
 	if attack_component is Enemy_Area_Spawner:
 		attack_component.spawn_area(stats.attack_pattern)
 		return
-		
+
 func get_damage():
-	print("get_damage")		
+	print("get_damage")
 	if anim != null:
 		anim.play("get_damage")
 
