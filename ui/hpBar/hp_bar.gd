98b7da090efa821bd4f85276d0661bd9d646e0cd
Lin1ux <liniuxpl2@gmail.com>
2025-10-06 21:25:41 +0200

    gownokod incomming

--- ui/hpBar/hp_bar.gd
+++ ui/hpBar/hp_bar.gd
@@ -9,8 +9,13 @@ var hp
 func _ready():
 	GlobalSignals.player_get_damage.connect(update_hp_bar)
 	init_health(GameInfo.player.HP.health)
+	
+func _process(delta: float) -> void:
+	if hp == 0:
+		init_health(GameInfo.player.HP.health)
 
 func init_health(value : int):
+	print("zajebie sie",value)
 	hp = value
 	hp_bar.max_value = hp
 	hp_bar.min_value = 0
@@ -20,9 +25,12 @@ func init_health(value : int):
 	damage_bar.value = value
 	
 func update_hp_bar():
+	print("UPDATE HP BAR")
 	hp_bar.value = GameInfo.player.HP.health
 	hp = hp_bar.value
 	timer.start()
+	print("HP",hp)
 
 func _on_timer_timeout() -> void:
 	damage_bar.value = hp
+	print("TIME OUT")
